5b75f06331101f4fb487f286b21d8274f787e569
Eivy Lopez <elopez16@miumg.edu.gt>
2023-05-11 19:21:27 -0600

    Mi primer comit

---
+++ src/Modelos/EmpleadosModel.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author eivyr
+ */
+public class EmpleadosModel {
+    String apellido;
+    String nombre;
+    int codigo;
+    String telefono;
+
+    public EmpleadosModel(String apellido, String nombre, int codigo, String telefono) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.telefono = telefono;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }      
+    
+}
